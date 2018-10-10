960666a37eec8f44f10c9737faecaa23c6ae5b0f
Trevor Lewis <tenzingtitan@gmail.com>
2018-10-10 13:12:46 -0700

    Fixed CSS

--- instagram/src/componets/SearchBar/SearchBar.js
+++ instagram/src/componets/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBar = () => {
     return (
-        <div>
+        <div className="search">
         <input type="text" placeholder="Search" />
       </div>
     )
